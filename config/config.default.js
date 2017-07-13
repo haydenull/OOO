9619c506a51b348e40be0bb1ae7aa4ee75e676eb
brooklyn1984 <brooklyn1984@qq.com>
2017-07-14 00:26:26 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1499962730630_3731';
+
+  // add your config here
+
+  return config;
+};
