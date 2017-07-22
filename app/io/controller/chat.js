75d7c2d725868c149a723def36abbd1d983c9509
brooklyn1984 <brooklyn1984@qq.com>
2017-07-23 00:20:30 +0800

    添加 debug 模式，实现群体发送消息

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -1,8 +1,11 @@
 'use strict';
 module.exports = app => {
     return function* () {
+        const self = this;
         const message = this.args[0];
         console.log('chat 控制器打印', message);
-        this.socket.emit('res', `Hi! I've got your message: ${message}`);
+        // this.socket.emit('res', `Hi! I've got your message: ${message}`);
+        // this.io.sockets.emit('res', 'test');
+        this.server.sockets.emit('res', 'test');
     };
 };
